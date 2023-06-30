2c2a2350dc0e66f586aaea39459a716b7ab2c8e8
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2023-06-30 13:18:59 -0300

    instalacion y configuracion de tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{tsx,jsx,js,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#181818',
+        grey: '#f2f2f2',
+        lightGrey: '#828282',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
